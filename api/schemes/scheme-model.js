1b0284fc69742c43cc13ae0325209d520a193262
Kaleb-Rupe <57547999+Kaleb-Rupe@users.noreply.github.com>
2023-09-03 18:28:54 -0400

    wip

--- api/schemes/scheme-model.js
+++ api/schemes/scheme-model.js
@@ -184,6 +184,14 @@ function addStep(scheme_id, step) {
     and resolves to _all the steps_ belonging to the given `scheme_id`,
     including the newly created one.
   */
+  return db("steps")
+    .insert({
+      ...step,
+      scheme_id,
+    })
+    .then(() => {
+      return db("steps").where("scheme_id", scheme_id);
+    });
 }
 
 module.exports = {
